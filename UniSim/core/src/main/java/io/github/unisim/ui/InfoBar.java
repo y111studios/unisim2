1245950dd8a21b79ef9d8748bf16ac22d379520f
Roboss11 <sam.knight0411@gmail.com>
2024-12-01 15:01:19 +0000

    Added demo money label to the info bar

--- UniSim/core/src/main/java/io/github/unisim/ui/InfoBar.java
+++ UniSim/core/src/main/java/io/github/unisim/ui/InfoBar.java
@@ -27,6 +27,7 @@ public class InfoBar {
   private Skin skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
   private Label scoreLabel = new Label("86%", skin);
   private Label titleLabel = new Label("UniSim", skin);
+  private Label moneyLabel = new Label("Money: $1000", skin);
   private Label timerLabel;
   private Texture pauseTexture = new Texture("ui/pause.png");
   private Texture playTexture = new Texture("ui/play.png");
@@ -36,6 +37,7 @@ public class InfoBar {
   private Cell<Label> timerLabelCell;
   private Cell<Label> scoreLabelCell;
   private Cell<Image> pauseButtonCell;
+  private Cell<Label> moneyLabelCell;
   private Cell<Table> buildingCountersTableCell;
   private Cell[] buildingCounterCells;
   private World world;
@@ -67,6 +69,7 @@ public class InfoBar {
     pauseButtonCell = infoTable.add(playImage).align(Align.center);
     timerLabelCell = infoTable.add(timerLabel).align(Align.center);
     scoreLabelCell = infoTable.add(scoreLabel).align(Align.center);
+    moneyLabelCell = infoTable.add(moneyLabel).align(Align.center);
     buildingCountersTableCell = infoTable.add(buildingCountersTable).expandX().align(Align.right);
 
     // Pause button
@@ -135,7 +138,10 @@ public class InfoBar {
     timerLabelCell.padLeft(height * 0.005f);
     scoreLabel.setFontScale(height * 0.002f);
     scoreLabelCell.width(height * 0.04f).height(height * 0.05f);
-    scoreLabelCell.padLeft(Math.min(width, height * 2) * 0.14f);
+    scoreLabelCell.padLeft(Math.min(width, height * 2) * 0.10f);
+    moneyLabel.setFontScale(height * 0.002f);
+    moneyLabelCell.width(height * 0.04f).height(height * 0.05f);
+    moneyLabelCell.padLeft(Math.min(width, height * 2) * 0.08f);
     pauseButtonCell.width(height * 0.03f).height(height * 0.03f)
     .padLeft(height * 0.01f).padRight(height * 0.01f);
 
