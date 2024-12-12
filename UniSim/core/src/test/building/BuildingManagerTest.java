644dfa80e0f9ec41c48c06f87a8b2be454c0f183
Tempestar11 <cxx504@york.ac.uk>
2024-12-12 22:44:52 +0000

    Added tests for constructor

--- UniSim/core/src/test/building/BuildingManagerTest.java
+++ UniSim/core/src/test/building/BuildingManagerTest.java
@@ -1,3 +1,25 @@
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.math.Matrix4;
+
+import io.github.unisim.building.BuildingManager;
+
+
 public class BuildingManagerTest {
 
+    private BuildingManager buildingManager;
+
+    @BeforeEach
+    public void setUp() {
+        Matrix4 isoTransform = new Matrix4();
+        buildingManager = new BuildingManager(isoTransform);
+    }
+
+    @Test
+    public void testConstructor() {
+        assertNotNull(buildingManager);
+        assertNotNull(buildingManager.getBuildings());
+    }
 }
