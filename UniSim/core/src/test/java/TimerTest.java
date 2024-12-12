40e4dffde306b260caef289e359e0837fd24b63c
Tempestar11 <cxx504@york.ac.uk>
2024-12-12 21:36:59 +0000

    Added test for the timer tick function

--- UniSim/core/src/test/java/TimerTest.java
+++ UniSim/core/src/test/java/TimerTest.java
@@ -12,6 +12,7 @@ public class TimerTest {
     //     assertTrue(true);
     // } */
 
+    //* Testing the constructor */
     @org.junit.jupiter.api.Test
     public void testConstructor() {
         timer = new Timer(5000);
@@ -36,8 +37,61 @@ public class TimerTest {
         assertFalse(timer.isRunning());
     }
 
+    //*Testing the timer tick method */
     @org.junit.jupiter.api.Test
     public void testTick() {
         timer = new Timer(5000);
+        timer.tick(1000);
+        assertTrue(timer.isRunning());
+        assertEquals("00:04", timer.getRemainingTime());
+        assertTrue(timer.isRunning());
     }
+
+    @org.junit.jupiter.api.Test
+    public void testTickToCompletion() {
+        timer = new Timer(5000);
+        timer.tick(5000);
+        assertFalse(timer.isRunning());
+        assertEquals("00:00", timer.getRemainingTime());
+        assertFalse(timer.isRunning());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testTickAfterCompletion() {
+        timer = new Timer(5000);
+        timer.tick(6000);
+        assertFalse(timer.isRunning());
+        assertEquals("00:00", timer.getRemainingTime());
+        assertFalse(timer.isRunning());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testTickWithNegativeDeltaTime() {
+        timer = new Timer(5000);
+        timer.tick(-1000);
+        assertTrue(timer.isRunning());
+        assertEquals("00:05", timer.getRemainingTime());
+        assertTrue(timer.isRunning());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testReset() {
+        timer = new Timer(5000);
+        timer.tick(1000);
+        timer.reset();
+        assertEquals(5000, timer.getInitialTime());
+        assertEquals("00:05", timer.getRemainingTime());
+        assertTrue(timer.isRunning());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testResetAfterCompletion() {
+        timer = new Timer(5000);
+        timer.tick(5000);
+        timer.reset();
+        assertEquals(5000, timer.getInitialTime());
+        assertEquals("00:05", timer.getRemainingTime());
+        assertTrue(timer.isRunning());
+    }
+
 }
