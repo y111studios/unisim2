7fac08982d6f1eac62687e030b46d4770f8c3177
Tempestar11 <cxx504@york.ac.uk>
2024-12-14 19:36:19 +0000

    Created leaderboardTest

--- UniSim/core/src/test/java/io/github/unisim/leaderboard/LeaderboardTest.java
+++ UniSim/core/src/test/java/io/github/unisim/leaderboard/LeaderboardTest.java
@@ -1,5 +1,36 @@
 package io.github.unisim.leaderboard;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+
 public class LeaderboardTest {
 
+    private Leaderboard leaderboard;
+    private static final String FILE_ADDRESS = "leaderboard.json";
+
+    @BeforeEach
+    // Delete the file before each test
+    public void setUp() {
+        FileHandle file = Gdx.files.local(FILE_ADDRESS);
+        if (file.exists()) {
+            file.delete();
+        }
+        leaderboard = new Leaderboard();
+    }
+
+    // Testing the constructor
+    @Test
+    public void testConstructorWithFile() {
+        // Create the file manually
+        FileHandle file = Gdx.files.local(FILE_ADDRESS);
+        file.writeString("[]", false);
+        leaderboard = new Leaderboard();
+        assertNotNull(leaderboard.entries());
+        assertTrue(file.exists());
+    }
 }
