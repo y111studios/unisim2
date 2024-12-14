adf2962bf21d070eedf7e68f9c32fde9fe0c6e0a
Tempestar11 <cxx504@york.ac.uk>
2024-12-14 16:03:34 +0000

    Added subtract money tests for MoneyTrackerTest

--- UniSim/core/src/test/java/MoneyTrackerTest.java
+++ UniSim/core/src/test/java/MoneyTrackerTest.java
@@ -26,44 +26,74 @@ public class MoneyTrackerTest {
         assertEquals(0, tracker.getMoney());
     }
 
-     // Testing the add money method
-     @org.junit.jupiter.api.Test
-     public void testaddMoney() {
-         // Add 100 to 100, expecting 200
-         tracker = new MoneyTracker(100);
-         tracker.addMoney(100);
-         assertEquals(200, tracker.getMoney());
+    // Testing the add money method
+    @org.junit.jupiter.api.Test
+    public void testaddMoney() {
+        // Add 100 to 100, expecting 200
+        tracker = new MoneyTracker(100);
+        tracker.addMoney(100);
+        assertEquals(200, tracker.getMoney());
  
-         // Add 100 to 0, expecting 100
-         tracker = new MoneyTracker(0);
-         tracker.addMoney(100);
-         assertEquals(100, tracker.getMoney());
-     }
+        // Add 100 to 0, expecting 100
+        tracker = new MoneyTracker(0);
+        tracker.addMoney(100);
+        assertEquals(100, tracker.getMoney());
+    }
  
-     @org.junit.jupiter.api.Test
-     public void testAddNegativeMoney() {
-         // Add -200 to 100, expectng 0
-         tracker = new MoneyTracker(100);
-         tracker.addMoney(-200);
-         assertEquals(0, tracker.getMoney());
+    @org.junit.jupiter.api.Test
+    public void testAddNegativeMoney() {
+        // Add -200 to 100, expectng no operation
+        tracker = new MoneyTracker(100);
+        tracker.addMoney(-200);
+        assertEquals(100, tracker.getMoney());
+    }
  
-         // Add -200 to 0, expecting 0
-         tracker = new MoneyTracker(0);
-         tracker.addMoney(-200);
-         assertEquals(0, tracker.getMoney());
-     }
+    @org.junit.jupiter.api.Test
+    public void testAddZeroMoney() {
+        // Add 0 to 100, expecting 100
+        tracker = new MoneyTracker(100);
+        tracker.addMoney(0);
+        assertEquals(100,tracker.getMoney());
  
-     @org.junit.jupiter.api.Test
-     public void testAddZeroMoney() {
-         // Add 0 to 100, expecting 100
-         tracker = new MoneyTracker(100);
-         tracker.addMoney(0);
-         assertEquals(100,tracker.getMoney());
- 
-         // Add 0 to 0, expecting 0
-         tracker = new MoneyTracker(0);
-         tracker.addMoney(0);
-         assertEquals(0,tracker.getMoney());
-     }
- 
+        // Add 0 to 0, expecting 0
+        tracker = new MoneyTracker(0);
+        tracker.addMoney(0);
+        assertEquals(0,tracker.getMoney());
+    }
+
+    // Test the subtract money method
+    @org.junit.jupiter.api.Test
+    public void testSubtractMoney() {
+        // Subracting 100 from 100, expecting 0
+        tracker = new MoneyTracker(100);
+        tracker.subtractMoney(100);
+        assertEquals(100, tracker.getMoney());
+
+        // Subtracting 200 from 100, expecting 0
+        tracker = new MoneyTracker(100);
+        tracker.subtractMoney(200);
+        assertEquals(0, tracker.getMoney());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSubtractZeroMoney() {
+        // Subtracting 0 from 0, expecting 0
+        tracker = new MoneyTracker(0);
+        tracker.subtractMoney(0);
+        assertEquals(0, tracker.getMoney());
+
+        // Subtracting 0 from 100, expecting 100
+        tracker = new MoneyTracker(100);
+        tracker.subtractMoney(0);
+        assertEquals(100, tracker.getMoney());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSubtractNegativeMoney() {
+        // Subtracting -100 from 100, expecting no operation
+        tracker = new MoneyTracker(100);
+        tracker.subtractMoney(-100);
+        assertEquals(100, tracker.getMoney());
+    }
+
 }
