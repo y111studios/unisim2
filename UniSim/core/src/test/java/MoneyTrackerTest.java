9dca9ca8c7dfd8b3322bd1b8425eb2149a016ad2
Tempestar11 <cxx504@york.ac.uk>
2024-12-14 15:54:53 +0000

    Added addMoney tests for MoneyTrackerTest

--- UniSim/core/src/test/java/MoneyTrackerTest.java
+++ UniSim/core/src/test/java/MoneyTrackerTest.java
@@ -26,4 +26,44 @@ public class MoneyTrackerTest {
         assertEquals(0, tracker.getMoney());
     }
 
+     // Testing the add money method
+     @org.junit.jupiter.api.Test
+     public void testaddMoney() {
+         // Add 100 to 100, expecting 200
+         tracker = new MoneyTracker(100);
+         tracker.addMoney(100);
+         assertEquals(200, tracker.getMoney());
+ 
+         // Add 100 to 0, expecting 100
+         tracker = new MoneyTracker(0);
+         tracker.addMoney(100);
+         assertEquals(100, tracker.getMoney());
+     }
+ 
+     @org.junit.jupiter.api.Test
+     public void testAddNegativeMoney() {
+         // Add -200 to 100, expectng 0
+         tracker = new MoneyTracker(100);
+         tracker.addMoney(-200);
+         assertEquals(0, tracker.getMoney());
+ 
+         // Add -200 to 0, expecting 0
+         tracker = new MoneyTracker(0);
+         tracker.addMoney(-200);
+         assertEquals(0, tracker.getMoney());
+     }
+ 
+     @org.junit.jupiter.api.Test
+     public void testAddZeroMoney() {
+         // Add 0 to 100, expecting 100
+         tracker = new MoneyTracker(100);
+         tracker.addMoney(0);
+         assertEquals(100,tracker.getMoney());
+ 
+         // Add 0 to 0, expecting 0
+         tracker = new MoneyTracker(0);
+         tracker.addMoney(0);
+         assertEquals(0,tracker.getMoney());
+     }
+ 
 }
