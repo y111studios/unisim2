678e8713f042f51dd06864d954e5405a33f101f0
Tempestar11 <cxx504@york.ac.uk>
2024-12-14 23:43:29 +0000

    Added initial score test for ScroreTrackerTest

--- UniSim/core/src/test/java/io/github/unisim/scoring/ScoreTrackerTest.java
+++ UniSim/core/src/test/java/io/github/unisim/scoring/ScoreTrackerTest.java
@@ -1,5 +1,27 @@
 package io.github.unisim.scoring;
 
+import java.time.Instant;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class ScoreTrackerTest {
 
+    private ScoreTracker scoreTracker;
+    private float score;
+    private Map<ScoringObject, Instant> lastUpdateTimes;
+
+    @BeforeEach
+    public void setUp() {
+        scoreTracker = new ScoreTracker();
+    }
+
+    // Tests initial score
+    @Test
+    public void testInitialScore() {
+        scoreTracker = new ScoreTracker();
+        assertEquals(0, scoreTracker.getScore());
+    }
 }
