b7db2f6fd88c27905cce9157f902c4592da908c3
Tempestar11 <cxx504@york.ac.uk>
2024-12-14 19:55:58 +0000

    Addes initialisation tests for satisfactionTrackerTest

--- UniSim/core/src/test/java/io/github/unisim/scoring/SatisfactionTrackerTest.java
+++ UniSim/core/src/test/java/io/github/unisim/scoring/SatisfactionTrackerTest.java
@@ -1,5 +1,20 @@
 package io.github.unisim.scoring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class SatisfactionTrackerTest {
 
+    private SatisfactionTracker satisfactiontracker;
+
+    @BeforeEach
+    public void setUp() {
+        satisfactiontracker = new SatisfactionTracker();
+    }
+
+    @Test
+    public void testInitialSatisfaction() {
+        assertEquals(0, satisfactiontracker.getSatisfaction());
+    }
 }
