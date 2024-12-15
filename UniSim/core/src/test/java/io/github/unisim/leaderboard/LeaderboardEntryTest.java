edbc94b96c55dc3b9ae831f6b3fec155bdf0b9ed
Tempestar11 <cxx504@york.ac.uk>
2024-12-15 08:51:41 +0400

    Added constructor tests to LeaderboardEntryTest

--- UniSim/core/src/test/java/io/github/unisim/leaderboard/LeaderboardEntryTest.java
+++ UniSim/core/src/test/java/io/github/unisim/leaderboard/LeaderboardEntryTest.java
@@ -1,5 +1,24 @@
 package io.github.unisim.leaderboard;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
 public class LeaderboardEntryTest {
 
+    private LeaderboardEntry entry;
+
+    @Test
+    public void testConstructorWithNoName() {
+        assertThrows(IllegalArgumentException.class, () -> new LeaderboardEntry("", 100));
+    }
+
+    @Test
+    public void testConstructorWithNegativeScore() {
+        assertThrows(IllegalArgumentException.class, () -> new LeaderboardEntry("Ben", -1));
+    }
+
+    @Test
+    public void testConstructorWithInvalidImputs() {
+        assertThrows(IllegalArgumentException.class, () -> new LeaderboardEntry("", -1));
+    }
 }
