ed9cf70ed4f1c9bd8f159be71d0389309c3718c6
Tempestar11 <cxx504@york.ac.uk>
2024-12-15 00:37:21 +0000

    Added constructor tests for BuildingTest

--- UniSim/core/src/test/java/io/github/unisim/building/BuildingTest.java
+++ UniSim/core/src/test/java/io/github/unisim/building/BuildingTest.java
@@ -1,5 +1,21 @@
 package io.github.unisim.building;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class BuildingTest {
 
+    private Building building;
+
+    // Testing constructor
+    @Test
+    public void testConstructor() {
+        Building building = new Building(null, 0.0f, null, null, null, false, BuildingType.RECREATION, "", 0, 50);
+        assertEquals(BuildingType.RECREATION, building.type);
+        assertEquals(50, building.cost);
+        assertEquals(false, building.flipped);
+        assertEquals(0, building.capacity);
+    }
 }
