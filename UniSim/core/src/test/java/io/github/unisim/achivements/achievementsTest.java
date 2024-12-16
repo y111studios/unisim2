f43269eb922cc5aa1a3fed556962ce253859adc0
Tempestar11 <cxx504@york.ac.uk>
2024-12-17 07:39:08 +0800

    Added constructor test for achievementsTest

---
+++ UniSim/core/src/test/java/io/github/unisim/achivements/achievementsTest.java
@@ -0,0 +1,30 @@
+package io.github.unisim.achivements;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import io.github.unisim.achievements.Achievement;
+import io.github.unisim.achievements.ScoreModifierTemplate;
+
+public class achievementsTest {
+    
+    // Testing constructor
+    @Test
+    public void testConstructor() {
+        String name = "Test";
+        String description = "Test description";
+        Instant unlockTime = Instant.EPOCH;
+        ScoreModifierTemplate functionTemplate = ScoreModifierTemplate.ADD;
+        float scoreModifierValue = 0;
+        float progress = 0;
+        boolean unlocked = false;
+        boolean hidden = false;
+
+        Achievement achievement = new Achievement(name, description, unlockTime, functionTemplate, scoreModifierValue, progress, unlocked, hidden);
+
+        assertEquals(name, achievement.getName());
+        assertEquals(description, achievement.getDescription());
+        assertEquals(unlocked, achievement.isUnlocked());
+    }
+}
