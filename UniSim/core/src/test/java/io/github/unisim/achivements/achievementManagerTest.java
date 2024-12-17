646787d5de8fa5855b35cb5f54837465fa55d531
Tempestar11 <cxx504@york.ac.uk>
2024-12-17 08:48:07 +0800

    Added getAchievements tests for achievementManagerTest

--- UniSim/core/src/test/java/io/github/unisim/achivements/achievementManagerTest.java
+++ UniSim/core/src/test/java/io/github/unisim/achivements/achievementManagerTest.java
@@ -4,13 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 
 import io.github.unisim.achievements.AchievementManager;
+
 public class achievementManagerTest {
 
     AchievementManager achievementManager;
+    
     // Testing constructor
     @Test
     public void testAchievementManager() {
         achievementManager = new AchievementManager();
         assertNotNull(achievementManager);
+    }
+
+    // Testing getAchievements
+    @Test
+    public void testGetAchievements() {
+        achievementManager = new AchievementManager();
+        assertNotNull(achievementManager.getAchievements());
+        System.out.println(achievementManager.getAchievements());
     }
 }
