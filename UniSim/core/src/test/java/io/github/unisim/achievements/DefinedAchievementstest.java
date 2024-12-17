c9e68078d1daf91feb07dd413c763d1dcb8f3d8e
Roboss11 <sam.knight0411@gmail.com>
2024-12-17 17:43:14 +0000

    Corrected to follow naming conventions

--- UniSim/core/src/test/java/io/github/unisim/achievements/DefinedAchievementstest.java
+++ UniSim/core/src/test/java/io/github/unisim/achievements/DefinedAchievementstest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
-public class DefinedAchievementstest {
+public class DefinedAchievementsTest {
 
     private AchievementManager achievementManager;
 
