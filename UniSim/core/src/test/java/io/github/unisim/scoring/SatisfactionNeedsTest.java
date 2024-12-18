d27d7facb56b6a279646d48c7f4fdd699e1799d7
Tempestar11 <cxx504@york.ac.uk>
2024-12-18 08:01:57 +0800

    Added complete tests for SatisfactionNeedsTest

--- UniSim/core/src/test/java/io/github/unisim/scoring/SatisfactionNeedsTest.java
+++ UniSim/core/src/test/java/io/github/unisim/scoring/SatisfactionNeedsTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.github.unisim.building.Building;
+import io.github.unisim.building.BuildingType;
 
 public class SatisfactionNeedsTest {
 
@@ -21,5 +22,45 @@ public class SatisfactionNeedsTest {
         List<Building> buildings = new ArrayList<>();
         float satisfaction = new SatisfactionNeeds().getSatisfaction(buildings, 50);
         assertEquals(0, satisfaction);
+    }
+
+    @Test
+    public void testGetSatisfactionWithZeroCapacityBuildings() {
+        List<Building> buildings = new ArrayList<>();
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.SLEEPING, "", 0, 0));
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.LEARNING, "", 0, 0));
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.EATING, "", 0, 0));
+        float satisfaction = new SatisfactionNeeds().getSatisfaction(buildings, 50);
+        assertEquals(0, satisfaction);
+    }
+
+    @Test
+    public void testGetSatisfactionWithSomeCapacityBuildings() {
+        List<Building> buildings = new ArrayList<>();
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.SLEEPING, "", 100, 0));
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.LEARNING, "", 100, 0));
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.EATING, "", 100, 0));
+        float satisfaction = new SatisfactionNeeds().getSatisfaction(buildings, 100);
+        assertEquals(1, satisfaction);
+    }
+
+    @Test
+    public void testGetSatisfactionWithZeroStudents() {
+        List<Building> buildings = new ArrayList<>();
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.SLEEPING, "", 100, 0));
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.LEARNING, "", 100, 0));
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.EATING, "", 100, 0));
+        float satisfaction = new SatisfactionNeeds().getSatisfaction(buildings, 0);
+        assertEquals(0, satisfaction);
+    }
+
+    @Test 
+    public void testGetSatisfactionWithMixedCapcities() {
+        List<Building> buildings = new ArrayList<>();
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.SLEEPING, "", 100, 0));
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.LEARNING, "", 50, 0));
+        buildings.add(new Building(null, 0.0f, null, null, null, false, BuildingType.EATING, "", 25, 0));
+        float satisfaction = new SatisfactionNeeds().getSatisfaction(buildings, 100);
+        assertEquals(0.25, satisfaction);
     }
 }
