114c05a9741716a0ef34e072e3333ded598c3c54
James <fjv501@york.ac.uk>
2024-12-28 14:36:38 +0000

    Added some buildings and a basic display of the cost of the buildings

--- UniSim/core/src/main/java/io/github/unisim/ui/BuildingMenuEntry.java
+++ UniSim/core/src/main/java/io/github/unisim/ui/BuildingMenuEntry.java
@@ -11,6 +11,7 @@ public class BuildingMenuEntry {
 
   private Table navTable;
   private Table buildingTable;
+  private Table costTable;
 
   /**
    * creates a new holder for the images and navigation
@@ -19,6 +20,7 @@ public class BuildingMenuEntry {
   public BuildingMenuEntry() {
     navTable = new Table();
     buildingTable = new Table();
+    costTable = new Table();
   }
 
   public void addToNavTable(Actor actor) {
@@ -29,14 +31,20 @@ public class BuildingMenuEntry {
     buildingTable.add(actor);
   }
 
+  public void addToCostTable(Actor actor) {
+    costTable.add(actor);
+  }
+
   public void addToStage(Stage stage) {
     stage.addActor(buildingTable);
     stage.addActor(navTable);
+    stage.addActor(costTable);
   }
 
   public void removeFromStage() {
     buildingTable.remove();
     navTable.remove();
+    costTable.remove();
   }
 
   public Table getNavTable() {
@@ -47,6 +55,10 @@ public class BuildingMenuEntry {
     return buildingTable;
   }
 
+  public Table getCostTable() {
+    return costTable;
+  }
+
   /**
    * Called when the window is resized, scales the building menu images with the window size.
 
@@ -55,18 +67,22 @@ public class BuildingMenuEntry {
    */
   @SuppressWarnings("unchecked")
   public void resize(int width, int height) {
-    buildingTable.setBounds(0, 0, width, height * 0.1f);
+    costTable.setBounds(0, 0, width, height * 0.025f);
+    buildingTable.setBounds(0, height * 0.015f, width, height * 0.1f);
     navTable.setBounds(0, height * 0.1f, width, height * 0.025f);
     // we must perform an unchecked type conversion here
     // this is acceptable as we know our table only contains instances of Actors
     for (Cell<Actor> cell : buildingTable.getCells()) {
-    Image buildingImage = (Image) (cell.getActor());
-    Vector2 textureSize = new Vector2(buildingImage.getWidth(), buildingImage.getHeight());
-    cell.width(
-        height * 0.1f * (textureSize.x < textureSize.y ? textureSize.x / textureSize.y : 1)
-    ).height(
-        height * 0.1f * (textureSize.y < textureSize.x ? textureSize.y / textureSize.x : 1)
-    );
+      Image buildingImage = (Image) (cell.getActor());
+      Vector2 textureSize = new Vector2(buildingImage.getWidth(), buildingImage.getHeight());
+      cell.width(
+          height * 0.1f * (textureSize.x < textureSize.y ? textureSize.x / textureSize.y : 1)
+      ).height(
+          height * 0.1f * (textureSize.y < textureSize.x ? textureSize.y / textureSize.x : 1)
+      );
+    }
+    for (Cell<Actor> cell : costTable.getCells()) {
+      cell.pad(width * 0.02f);
     }
   }
 }
