aef0d6a7f4a164c9f188c812568cf93916b78068
Tempestar11 <cxx504@york.ac.uk>
2025-01-08 13:45:48 +0800

    Updated choiceCardTest

--- UniSim/core/src/test/java/io/github/unisim/events/choiceCardTest.java
+++ UniSim/core/src/test/java/io/github/unisim/events/choiceCardTest.java
@@ -1,125 +1,102 @@
-package io.giyhub.unisim.events;
+package io.github.unisim.events;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-import io.github.unisim.finance.moneyTracker;
+import io.github.unisim.achievements.ScoreModifierTemplate;
+import io.github.unisim.finance.MoneyTracker;
+import io.github.unisim.scoring.SatisfactionTracker;
 
 public class choiceCardTest {
+
+    MoneyTracker moneyTracker;
+    ChoiceCard card;
+
 
     // Testing constructor
     @Test
     public void testConstructor() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
-        assertEquals("Test", card.getName());
+        ChoiceCard card = new ChoiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
+        assertEquals("Test", card.getTitle());
         assertEquals("Test Desc", card.getDescription());
         assertEquals(1, card.getMoneyEffect());
-        assertEquals(1, card.getScore());
-        assertEquals(ScoreModifierTemplate.ADD, card.getScoreModifier());
+        assertEquals(ScoreModifierTemplate.ADD, card.getSatisfactionModifier());
     }
 
     // Testing getTitle
     @Test
     public void testGetTitle() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
-        assertEquals("Test", card.getName());
+        ChoiceCard card = new ChoiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
+        assertEquals("Test", card.getTitle());
     }
 
     @Test
     public void testGetNullTitle() {
-        choiceCard card = new choiceCard(null, "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
-        assertEquals(null, card.getName());
+        ChoiceCard card = new ChoiceCard(null, "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
+        assertEquals(null, card.getTitle());
     }
 
     // Testing getDescription  
     @Test
     public void testGetDescription() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
+        ChoiceCard card = new ChoiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
         assertEquals("Test Desc", card.getDescription());
     }
 
     @Test
     public void testGetNullDescription() {
-        choiceCard card = new choiceCard("Test", null, 1, ScoreModifierTemplate.ADD, 1);
+        ChoiceCard card = new ChoiceCard("Test", null, 1, ScoreModifierTemplate.ADD, 1);
         assertEquals(null, card.getDescription());
     }
 
     // Testing getMoneyEffect
     @Test
     public void testGetMoneyEffect() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
+        ChoiceCard card = new ChoiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
         assertEquals(1, card.getMoneyEffect());
     }
 
     @Test
     public void testGetNegativeMoneyEffect() {
-        choiceCard card = new choiceCard("Test", "Test Desc", -1, ScoreModifierTemplate.ADD, 1);
+        ChoiceCard card = new ChoiceCard("Test", "Test Desc", -1, ScoreModifierTemplate.ADD, 1);
         assertEquals(-1, card.getMoneyEffect());
     }
 
     @Test
     public void testGetZeroMoneyEffect() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 0, ScoreModifierTemplate.ADD, 1);
+        ChoiceCard card = new ChoiceCard("Test", "Test Desc", 0, ScoreModifierTemplate.ADD, 1);
         assertEquals(0, card.getMoneyEffect());
-    }
-
-    @Test
-    public void testGetNullMoneyEffect() {
-        choiceCard card = new choiceCard("Test", "Test Desc", null, ScoreModifierTemplate.ADD, 1);
-        assertEquals(null, card.getMoneyEffect());
-    }
-
-    // Testing getScore
-    @Test
-    public void testGetScore() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
-        assertEquals(1, card.getScore());
-    }
-
-    @Test
-    public void testGetNegativeScore() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, -1);
-        assertEquals(-1, card.getScore());
-    }
-
-    @Test
-    public void testGetZeroScore() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 0);
-        assertEquals(0, card.getScore());
-    }
-
-    @Test
-    public void testGetNullScore() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, null);
-        assertEquals(null, card.getScore());
     }
 
     // Testing getScoreModifier
     @Test
     public void testGetScoreModifier() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
-        assertEquals(ScoreModifierTemplate.ADD, card.getScoreModifier());
+        ChoiceCard card = new ChoiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
+        assertEquals(ScoreModifierTemplate.ADD, card.getSatisfactionModifier());
     }
 
     @Test
     public void testGetNullScoreModifier() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 1, null, 1);
-        assertEquals(null, card.getScoreModifier());
+        ChoiceCard card = new ChoiceCard("Test", "Test Desc", 1, null, 1);
+        assertEquals(null, card.getSatisfactionModifier());
     }
 
     // Testing applyEffect
     @Test
     public void testApplyEffect() {
-        choiceCard card = new choiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
-        moneyTracker money = new moneyTracker(0);
-        card.applyEffect(money);
+        ChoiceCard card = new ChoiceCard("Test", "Test Desc", 1, ScoreModifierTemplate.ADD, 1);
+        MoneyTracker money = new MoneyTracker(0);
+        SatisfactionTracker satisfaction = new SatisfactionTracker();
+        card.applyEffects(money, satisfaction);
         assertEquals(1, money.getMoney());
     }
 
     @Test
     public void testApplyNegativeEffect() {
-        choiceCard card = new choiceCard("Test", "Test Desc", -1, ScoreModifierTemplate.ADD, 1);
-        moneyTracker money = new moneyTracker(1);
-        card.applyEffect(money);
+        ChoiceCard card = new ChoiceCard("Test", "Test Desc", -1, ScoreModifierTemplate.ADD, 1);
+        MoneyTracker money = new MoneyTracker(1);
+        SatisfactionTracker satisfaction = new SatisfactionTracker();
+        card.applyEffects(money, satisfaction);
         assertEquals(0, money.getMoney());
     }
 
